823a511e26562dce80e771eedc71be633a6dcf13
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-06 04:11:59 +0100

    initialize the parent father component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+import NavBar from "./NavBar";
+import styles from "./header.module.css";
+
+function Header() {
+  const [theme, setTheme] = useState("light");
+  const [toogle, setToggle] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, []);
+
+  return (
+    <header className={styles.header}>
+      <NavBar />
+      <ThemeToggle />
+      <div className={styles.right}>
+        <Link className={styles.btn} href="/login">
+          Login
+        </Link>
+        <Link className={styles.btn} href="/register">
+          Register
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
