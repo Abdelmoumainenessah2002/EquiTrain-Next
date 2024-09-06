168b4fdf695ed2a3187e016484c1771fcc132a39
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-06 04:10:40 +0100

    add the color of texts and backgrounds in the light and dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,22 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // Enables dark mode using a class
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: {
+          light: "#f3f4f6", // Light mode background
+          dark: "#121212", // Dark mode background
+        },
+        text: {
+          light: "#111827", // Light mode text
+          dark: "#f9fafb", // Dark mode text
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
