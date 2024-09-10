6f0c44415c522e37aeb1a5464bd4aad47d6aa11f
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-10 23:24:41 +0100

    initialize the hero section of the unauthenticated user interdace

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,24 @@
 import { useTranslations } from "next-intl";
-import { Link } from "@/i18n/routing";
+import  Image  from 'next/image'
+import HomeImg from '../../../public/home-img.webp'
+
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
-    <div>
-      
-    </div>
+    <section className="man-container">
+      <div className="flex items-center justify-between flex-col text-center pt-5 h-full">
+        <h1 className="text-2xl font-bold my-6 md:font-semibold md:text-4xl lg:text-5xl">Unlock Your Horse&apos;s Full Potential</h1>
+        <p className="text-[11px] font-normal leading-snug px-4 mb-5 md:text-[13px]">
+          Welcome to EquiTrain, your premier destination for horse training
+          excellence. We offer top-notch horse coaching, rider training, and
+          equine education resources to help you unlock your horse’s full
+          potential. Whether you’re a beginner or an experienced equestrian, our
+          expert advice and supportive community will help you achieve your
+          goals.
+        </p>
+        <Image src={HomeImg} alt="home image" height={5600} width={1200} />
+      </div>
+    </section>
   );
 }
